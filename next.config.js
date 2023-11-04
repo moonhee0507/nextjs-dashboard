2d33d30fd3ded0e3b82f24475fbf0aec99fc7efa
moonhee0507 <sjmh0507@naver.com>
2023-11-04 22:46:38 +0900

    middleware.ts setting

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'nextjs-dashboard-chi-rust.vercel.app',
-                port: '',
-                pathname: '/dashboard/**',
-            },
-        ],
-    },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
